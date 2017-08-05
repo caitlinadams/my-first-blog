de39fc452f57932126db2ad246a155e5f1ac31ba
Caitlin Adams <caitlin.isabel.adams@gmail.com>
2017-08-05 16:09:15 +1000

    Added view and templae for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 #name corresponds to the url that identifies the view
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
